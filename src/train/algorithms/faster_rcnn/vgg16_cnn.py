b0f3be78c823807972fc677b80c8ec2db838ccc3
cfont03 <cfont03@uoc.edu>
2022-06-03 11:21:27 +0200

    commit

---
+++ src/train/algorithms/faster_rcnn/vgg16_cnn.py
@@ -0,0 +1,87 @@
+import time
+import cv2
+import numpy as np
+import tensorflow as tf
+from keras import Model
+
+
+def vgg16_cnn(df, h = 800, w = 800, c = 3):
+
+  '''
+
+  This function uses pre-trained vgg16 model to extract the feature map of the passed images. 
+
+  Params: df with
+  0: image path
+
+  Returns: 
+  - output model of vgg16
+  - feature map
+  - df with additional info:
+  w_fm: width of the feature map
+  h_fm: height of the feature map
+  n_anchors: number of potential anchors per image
+
+  '''
+
+  # show execution time
+  start_time = time.time()
+
+
+
+  # --------------- vgg16 model -------------
+   
+  vgg16 = tf.keras.applications.VGG16(
+    include_top=False, 
+    weights='imagenet',
+    input_shape = (w, h, c)
+  )
+
+  for layer in vgg16.layers:
+    layer.trainable = True
+  
+  vgg16_model = Model(inputs= [vgg16.layers[0].input], outputs= [vgg16.layers[17].output])
+
+
+  # train data
+  train_images = []
+  
+  for i in df.iloc:
+    img = cv2.imread(str(i[0]))
+    train_images.append(img)
+
+  train_images = np.array(train_images)
+  train_images = train_images/255 # normalize images
+
+  feature_map = vgg16_model.predict(train_images)
+
+
+  # feature map
+  anchors = []
+  w_fms = []
+  h_fms = []
+  features = []
+
+  for i in df.iloc:
+    img = cv2.imread(str(i[0]))
+    fm = vgg16_model.predict(np.expand_dims(img, 0))
+    _, w_fm, h_fm, _ = fm.shape
+    n_anchor = w_fm * h_fm  
+    anchors.append(n_anchor)
+    w_fms.append(w_fm)
+    h_fms.append(h_fm)
+
+
+
+  df['n_anchor'] = anchors
+  df['w_fm'] = w_fms
+  df['h_fm'] = h_fms 
+
+
+
+  print(f"\n------- Execution time: {(time.time() - start_time)/60:.2f} minutes -------\n")
+  print('Number of anchors needed: ', n_anchor)
+  print('\n', vgg16_model.summary())
+  
+  
+  return df, vgg16_model, feature_map
---
+++ src/train/utils/plots.py
@@ -0,0 +1,87 @@
+import cv2
+from pathlib import Path
+from matplotlib import pylab as plt
+from matplotlib import patches
+
+
+def boundary_box(df):
+  
+  '''
+
+  This function plots a given image with its boundary boxes.
+
+  Args: df with Params:
+  1: name image
+  8: path image
+  9: min coord x
+  10: max coord x
+  11: min coord y
+  12: max coord y
+
+  Outputs: plot given image with boundary box
+
+  '''
+
+  image = input("introduce image name: ") 
+  path = 'res/all-mias/{:}'.format(image)
+  im = cv2.imread(path)
+  
+  # generate x, y, width and height
+  xmin = df[df['image path'].apply(lambda x : x == str(Path(path)))].iloc[:,9]
+  xmax = df[df['image path'].apply(lambda x : x == str(Path(path)))].iloc[:,10]
+  ymin = df[df['image path'].apply(lambda x : x == str(Path(path)))].iloc[:,11]
+  ymax = df[df['image path'].apply(lambda x : x == str(Path(path)))].iloc[:,12]
+  
+  ### CHECKPOINT INPUT
+  if ( len(xmin) == 0 or len(xmax) == 0 or len(ymin) == 0 or len(ymax) == 0 ):
+      print("ERROR: image is not in training dataset!")
+  else:
+      pass
+  
+  xmin, xmax, ymin, ymax = (xmin.values[0], xmax.values[0], ymin.values[0], ymax.values[0])
+
+  ### CHECKPOINT INPUT
+  if (xmin > xmax or ymin > ymax):
+      print("ERROR: coordinates are wrong!")
+  else:
+      pass
+  
+  width = xmax - xmin
+  height = ymax - ymin
+
+  # Create figure and axes
+  fig, ax = plt.subplots()
+
+  ax.imshow(im)
+
+  rect = patches.Rectangle((xmin, ymin), width, height, linewidth=1, edgecolor='b', facecolor='none')
+
+  ax.add_patch(rect)
+
+  plt.show()
+
+
+def plot_loss_rpn(hist):
+    plt.plot(hist.history['rpn_out_score_loss'], 'b')
+    plt.style.use('seaborn')
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train'], loc='upper right')
+    plt.show()
+
+def plot_loss_yolo(hist):
+    plt.plot(hist.history['loss'], 'b')
+    plt.style.use('seaborn')
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train'], loc='upper right')
+    plt.show()
+
+def plot_map(precisions, recalls):
+    plt.plot(precisions, recalls, linewidth=3, color="red")
+    plt.xlabel("Recall", fontsize=12, fontweight='bold')
+    plt.ylabel("Precision", fontsize=12, fontweight='bold')
+    plt.title("Precision-Recall Curve", fontsize=15, fontweight="bold")
+    plt.show()
